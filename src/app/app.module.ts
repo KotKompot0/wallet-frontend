a2c5be5e733fccbd80f8d341f6fade3ec335d933
K07_K0mp07 <62478487+KotKompot0@users.noreply.github.com>
2023-09-21 14:30:16 +0000

    chore: added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BankAccountInfoComponent } from './dashboard/components/bank-account-info/bank-account-info.component';
+import { SidebarMenuComponent } from './dashboard/components/sidebar-menu/sidebar-menu.component';
+import { MonthlyStatsComponent } from './dashboard/components/monthly-stats/monthly-stats.component';
+import { ReportCardComponent } from './dashboard/components/report-card/report-card.component';
+import { GroupedBarGraphComponent } from './dashboard/components/grouped-bar-graph/grouped-bar-graph.component';
+import { TransactionHistoryComponent } from './dashboard/components/transaction-history/transaction-history.component';
+import { TransactionHistoryItemComponent } from './dashboard/components/transaction-history-item/transaction-history-item.component';
+// import randomcolor  from 'randomcolor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BankAccountInfoComponent,
+    SidebarMenuComponent,
+    MonthlyStatsComponent,
+    ReportCardComponent,
+    GroupedBarGraphComponent,
+    TransactionHistoryComponent,
+    TransactionHistoryItemComponent
   ],
   imports: [
     BrowserModule,
